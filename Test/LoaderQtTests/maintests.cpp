ba95736c8882f2e78abf241ecb8040455aff4339
astimanuka <asti.manuka@gmail.com>
2016-07-06 12:07:58 +0200

    added files from google test framework , added a test file

---
+++ Test/LoaderQtTests/maintests.cpp
@@ -0,0 +1,38 @@
+#include<gtest/gtest.h>
+#include "../../src/files.h"
+#include <iostream>
+using namespace std;
+
+
+TEST(testFilesClass, testSetterAndGetter_1){
+
+    //fexpected to fail
+    Files* actual= new Files();
+    Files* actual1= new Files();
+    actual1->setPath("mypath1");
+    actual->setPath("mypath");
+
+
+
+    ASSERT_EQ(actual1->getPath(),actual->getPath());
+
+}
+TEST(testFilesClass,testSetterAndGetterPath_1){
+
+    Files* myfile= new Files();
+    myfile->setPath("/home/astimanuka/Desktop/main.cpp");
+
+    Files* myfile2= new Files();
+    myfile2->setPath("/home/astimanuka/Desktop/main.cpp");
+
+    ASSERT_EQ(myfile2->getPath(),myfile->getPath());
+}
+
+
+
+int main(int argc,char* argv[]){
+
+    ::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+
+}
