17d2469c2e9ae76047fca13d954b37b6a12dd22a
astimanuka <asti.manuka@gmail.com>
2016-07-11 18:47:58 +0200

    added the test for the progressive upload

--- Test/LoaderQtTests/maintests.cpp
+++ Test/LoaderQtTests/maintests.cpp
@@ -64,7 +64,7 @@ TEST(testFileLoaderClass,testGetTotalFileSize){
 
 //--------------------------------testing the progressbar class ------------------------
 
-TEST(testProgressBarClass, testGetPath){
+TEST(testProgressBarClass, testGetTotalSize){
 
     FileLoader* fileLoader= new FileLoader();
     ProgressBar* progress=new ProgressBar(fileLoader);
@@ -83,6 +83,36 @@ TEST(testProgressBarClass, testGetPath){
     }
 
     ASSERT_EQ(37038,progress->getTotalSize());
+}
+
+TEST(testProgressBarClass, testProgressiveLoad){
+
+    FileLoader* fileLoader= new FileLoader();
+    ProgressBar* progress=new ProgressBar(fileLoader);
+
+
+    Files* actual= new Files("/home/astimanuka/Desktop/test_uploader/Derived1.cpp");
+    Files* actual1= new Files("/home/astimanuka/Desktop/test_uploader/Derived2.cpp");
+    Files* actual2= new Files("/home/astimanuka/Desktop/test_uploader/DerivedAbstractClass.cpp");
+    Files* actual3= new Files("/home/astimanuka/Desktop/test_uploader/IntMaxFunctor.cpp");
+    Files* actual4= new Files("/home/astimanuka/Desktop/test_uploader/main.cpp");
+
+    fileLoader->putFiles(*actual);
+    fileLoader->putFiles(*actual1);
+    fileLoader->putFiles(*actual2);
+    fileLoader->putFiles(*actual3);
+    fileLoader->putFiles(*actual4);
+
+    cout<<"\t\nTesting the loading of files :\n"<<endl;
+    int actualProgress=0;
+    int actualFileSizeLoad=fileLoader->getTotalFileSize();
+    for(int i=0;i<fileLoader->getFileCounter();i++){
+        progress->update((fileLoader->getFileInPosition(i)));
+        actualProgress++;
+        cout<<"\tLoad : "<<actualProgress<<"/"<<fileLoader->getFileCounter();
+        cout<<" files , memory load : "<<progress->getTotalSize()<<"/"<<actualFileSizeLoad<<" bytes" <<endl;
+    }
+    ASSERT_EQ(146659,progress->getTotalSize());
 }
 
 
