8d3aa165b50e8ca2b5ec5eda6ad4ed3fe340e1d0
astimanuka <asti.manuka@gmail.com>
2016-07-15 20:35:29 +0200

    added test for the progressive upload on the maintest file

--- Test/LoaderQtTests/maintests.cpp
+++ Test/LoaderQtTests/maintests.cpp
@@ -67,31 +67,10 @@ TEST(testFileLoaderClass,testGetTotalFileSize){
 
 //--------------------------------testing the progressbar class ------------------------
 
-TEST(testProgressBarClass, testGetTotalSize){
-
-    FileLoader* fileLoader= new FileLoader();
-    ProgressBar* progress=new ProgressBar(fileLoader);
-
-
-    Files* actual= new Files("/home/astimanuka/Desktop/main.cpp");
-    Files* actual1= new Files("/home/astimanuka/Desktop/main.cpp");
-    Files* actual2= new Files("/home/astimanuka/Desktop/main.cpp");
-
-    fileLoader->putFiles(*actual);
-    fileLoader->putFiles(*actual1);
-    fileLoader->putFiles(*actual2);
-
-    for(int i=0;i<fileLoader->getFileCounter();i++){
-        progress->update((fileLoader->getFileInPosition(i)));
-    }
-
-    ASSERT_EQ(37038,progress->getTotalSize());
-}
-
 TEST(testProgressBarClass, testProgressiveLoad){
 
     FileLoader* fileLoader= new FileLoader();
-    ProgressBar* progress=new ProgressBar(fileLoader);
+
 
 
     Files* actual= new Files("/home/astimanuka/Desktop/test_uploader/Derived1.cpp");
@@ -106,16 +85,12 @@ TEST(testProgressBarClass, testProgressiveLoad){
     fileLoader->putFiles(*actual3);
     fileLoader->putFiles(*actual4);
 
+    ProgressBar* myProgress=new ProgressBar(fileLoader);
+
     cout<<"\t\nTesting the upload progress of files :\n"<<endl;
-    int actualProgress=0;
-    int actualFileSizeLoad=fileLoader->getTotalFileSize();
-    for(int i=0;i<fileLoader->getFileCounter();i++){
-        progress->update((fileLoader->getFileInPosition(i)));
-        actualProgress++;
-        cout<<"\tLoad : "<<actualProgress<<"/"<<fileLoader->getFileCounter();
-        cout<<" files , memory load : "<<progress->getTotalSize()<<"/"<<actualFileSizeLoad<<" bytes" <<endl;
-    }
-    ASSERT_EQ(146659,progress->getTotalSize());
+
+    myProgress->updateProgressTest();
+    ASSERT_EQ(146659,myProgress->getTotalSize());
 }
 
 
